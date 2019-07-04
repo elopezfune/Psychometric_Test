6f35ab0a81819b2b849eb53278da0352eb5683b6
Ernesto Lopez Fune <elpezfun@lpnhe.in2p3.fr>
2019-07-04 14:07:14 +0200

    Add files via upload
    Psychometric testing is designed to fin job-relevant information about an applicant that the traditional interview process would not uncover. It typically includes a combination of online and personality tests that measure cognitive ability and personality traits.
    
    A company has psychometric scores for n candidates, and it wil only extend job offers to candidates with scores in the inclusive range given by [lowerLimit, upperLimit]. Given the list of scores and a sequence of score ranges, determine how many candidates the company will extend offers to for each range of scores. For example, if the scores are scores = [1,2,2,3,4] and the limits are 2 and 4, then there are 4 candidates, i.e. those with scores [2,2,3,4] that match the conditions.
    
    Build a function called jobOffes such that it must return an array of q integers where the value at each index i denotes the number of candidates in the inclusive range [lowerLimits[i],upperLimits[i]] that the company will extend offers to.

---
+++ Psychometric_test.py
@@ -0,0 +1,56 @@
+
+# coding: utf-8
+
+# In[8]:
+
+
+def jobOffers(scores,lowerLimits,upperLimits):
+    if len(scores)>10**5 or len(lowerLimits)>10**5 or len(lowerLimits) != len(upperLimits):
+        print('The arrays do not match the constrains')
+    else:
+        result = []
+        scores.sort()
+        n = len(scores)
+        for index, _ in enumerate(lowerLimits):
+            result.append(countInRange(scores, n, lowerLimits[index], upperLimits[index]))
+    return result
+
+# Function to find first index larger or equal than x
+def minIndex(array, n, x):
+    l = 0
+    h = n-1
+    while (l <= h):
+        mid = int((l + h)/2)
+        if (array[mid] >= x):
+            h = mid - 1
+        else:
+            l = mid + 1
+    return l
+ 
+
+# Function to find last index smaller or equal than x
+def maxIndex(array, n, x):
+    l = 0
+    h = n-1
+    while (l <= h):
+        mid = int((l + h)/2)
+        if (array[mid] <= x):
+            l = mid + 1
+        else:
+            h = mid - 1
+    return h
+ 
+ 
+# function to count elements within given range
+def countInRange(array, n, x, y):
+    # initialize result
+    count = 0;
+    count = maxIndex(array, n, y) - minIndex(array, n, x) + 1;
+    return count
+
+
+# In[9]:
+
+
+print(jobOffers([1,3,5,6,8],[2],[6]))
+
